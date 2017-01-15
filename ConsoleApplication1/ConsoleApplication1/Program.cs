fec02408e7dde842b5c14ef07d8b8ec4e6f3fefd
OLKOl\OLKO <olly.olko@gmail.com>
2017-01-15 21:20:38 +0200

    jjjgb kijutb

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Dynamic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Yellow;
+            SqlConnection connection = new SqlConnection();
+
+            connection.ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+            string commandText = @"CREATE DATABASE [AdoNetExample]
+                                 COLLATE Latin1_General_CI_AS";
+
+            //string commandText = @"
+            //use[AdoNetExample]
+            //create table UserInfo
+            //(
+            //    Id int identity,
+                
+            //)";
+
+            SqlCommand command=new SqlCommand(commandText, connection);
+            connection.Open();
+
+            int updateRows = command.ExecuteNonQuery();
+            if (updateRows == 0)
+            {
+                throw new DataException("Zero rows are affected");
+            }
+            connection.Close();
+            Console.WriteLine("well... done");
+            Console.ReadKey();
+        }
+    }
+}
